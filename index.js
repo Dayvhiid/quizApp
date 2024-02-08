ed8799688b03fb537d67cd7bba7afc385e63d3b4
Dada <daviddada360@gmail.com>
2024-02-08 09:47:52 +0100

    added counters with the state number for users to track thier progress

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
+let stateNumber = 0;
  //setting the control buttons
- console.log("Its working")
  const startBtn = document.getElementById('startBtn');
  const prevBtn = document.getElementById('prevBtn');
  const nextBtn = document.getElementById('nextBtn');
- let stateNumber = 0;
-
+ //statenumber in the dom
+ const quesNumber = document.getElementById('qn');
+ const questionCounterLabel = document.getElementById('qcLabel');
+ const questionCounter = document.getElementById('qc');
  if(stateNumber == 0){
      prevBtn.style.display = "none";
      nextBtn.style.display = "none";
+     questionCounterLabel.style.display = "none";
  } 
 
 startBtn.addEventListener("click", function (){
     prevBtn.style.display = "block";
     nextBtn.style.display = "block";
     startBtn.style.display = "none";
+    questionCounterLabel.style.display = "block";
 })
 
 nextBtn.addEventListener("click", function () {
    stateNumber++;
+   quesNumber.textContent = stateNumber;
+   questionCounter.textContent = stateNumber;
 })
 
 prevBtn.addEventListener("click", function (){
-   prevBtn--; 
+   stateNumber--; 
+   console.log("prev button pressed");
+   quesNumber.textContent = stateNumber;
+   questionCounter.textContent = stateNumber;
 })
